a9e211b94b92e83871cf852c60b6d65013b6500b
metop <servermeteor@gmail.com>
2025-05-25 13:39:33 +0300

    changed file types from lua to luau

--- src/ReplicatedStorage/Settings/CharacterMovement.lua
+++
@@ -1,7 +0,0 @@
-return {
-    DashSpeed = 300,
-    DashDuration = 0.3,
-    SpecialJumpStrength = 80,
-    MaxDashes = 3,
-    DashUpwardBoost = 30,
-}
--- src/StarterPlayer/StarterPlayerScripts/DisableDefaultControls.client.lua
+++
@@ -1,7 +0,0 @@
-local player = game.Players.LocalPlayer
-local playerScripts = player:WaitForChild("PlayerScripts")
-local playerModule = playerScripts:WaitForChild("PlayerModule")
-local controlModule = require(playerModule:WaitForChild("ControlModule"))
-
--- Disable the default controls
-controlModule:Disable()
